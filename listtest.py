ea2065ef6b334204a6bcfbc84865fb04e755d71c
Folks <ftdewit@home.nl>
2025-02-02 13:53:07 +0100

    Bookbot now counts characters

---
+++ listtest.py
@@ -0,0 +1,6 @@
+test_string = "This is a test string"
+
+for char in test_string:
+    print(char)
+
+
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    #print(file_contents)
+    print(word_count(file_contents))
+    print(letter_count(file_contents))
+
+def word_count(text):
+     return len(text.split())
+
+def letter_count(text):
+    lowercase_text = text.lower()
+    count = {}
+    for char in lowercase_text:
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 1
+    return count   
+
 
 main()
